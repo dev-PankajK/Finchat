fa94f408391c3503cb79c49b527b1375105c321d
KBI <>
2024-01-17 16:32:02 +0530

    added pytest_fixer and mysql support in workflow

---
+++ fin_platform/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from finbot_platform.db.connection import Connection
+from finbot_platform.settings import settings
+from finbot_platform.db.models.user import User
+from sqlmodel import SQLModel
+from sqlalchemy import create_engine
+is_table_exist = False
+@pytest.fixture(scope="session")
+def _engine():
+    """
+    Create engine and databases.
+
+    :yield: new engine.
+    """
+    from finbot_platform.db.models import load_all_models
+    engine = create_engine(settings.db_url, echo=True)
+    with engine.connect() as connection:
+        if engine.dialect.has_table(connection, User.__table__.name):
+            global is_table_exist
+            is_table_exist = True
+            connection.close()
+    if not is_table_exist:
+        with Connection(settings.db_url) as db:
+            print(f"INFO: User table does not exist creating all the tables.....")
+            load_all_models()
+            SQLModel.metadata.create_all(db.engine)
+            print(f"INFO: Successfully created all the tables.")
+    # Now check the user table existance and if there is no table than create all tables in the database
+    try:
+        yield {
+            "test": "ok"
+        }
+    finally:
+        engine.dispose()
--- fin_platform/finbot_platform/tests/db/test_models.py
+++ fin_platform/finbot_platform/tests/db/test_models.py
@@ -1,14 +1,16 @@
-from dotenv import load_dotenv
-load_dotenv(override=True)
 from finbot_platform.db.crud.user import create_user
 from finbot_platform.schemas.auth import CreateUserRequest
 from finbot_platform.db.models.user import User
+
+
+
 test_user = {
     "username": "abc.cdf@gmail.com",
     "password": "test123"
 }
 
-def test_create_a_user():
+def test_create_a_user(_engine):
+    assert _engine["test"] == "ok"
     create_test_user = CreateUserRequest(**test_user)
     user = create_user(create_test_user)
     assert isinstance(user,User)
